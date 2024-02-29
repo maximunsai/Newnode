ee9611283c1fac5598cb9306a1196f7423bb3dfb
maximunsai <123230870+maximunsai@users.noreply.github.com>
2024-02-29 17:14:45 +0530

    Update index.js1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const users = require('./MOCK_DATA.json');
-const port = 8080;
+const port = 8000;
 
 app.use(express.urlencoded({ extended: false}));
 
